4f78ae8aaab6cfc4fb74b8312445b4164fbad28d
Runtao Song <rs3799@columbia.edu>
2019-12-01 19:03:07 +0000

    import

--- finalproject/map/views.py
+++ finalproject/map/views.py
@@ -4,8 +4,6 @@ from django.shortcuts import render
 from django.http import HttpResponse
 from django.template import loader
 
-from .models import Question
-
 
 def index(request):
     latest_question_list = Question.objects.order_by('-pub_date')[:5]
--- finalproject/sightings/urls.py
+++ finalproject/sightings/urls.py
@@ -2,8 +2,7 @@ from django.contrib import admin
 from django.urls import include, path
 from . import views
 urlpatterns = [
-    path('', views.GET,name = 'sightings-get'),
-    path('add',views.POST,name = 'sightings-add'),
-    path('stats/' views.GET,name = 'sightings-stats'),
-    path('admin/', admin.site.urls),
+    path('', views.GET, name = 'sightings-get'),
+    path('add',views.POST, name = 'sightings-add'),
+    path('stats/', views.GET, name = 'sightings-stats'),
 ]
