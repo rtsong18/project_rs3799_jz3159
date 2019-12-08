a567dc5e4b61fdefed729d0cf2c60b71253d6319
Runtao Song <rs3799@columbia.edu>
2019-12-08 19:26:00 +0000

    models

--- finalproject/map/views.py
+++ finalproject/map/views.py
@@ -3,14 +3,11 @@ from django.shortcuts import render
 # Create your views here.
 from django.http import HttpResponse
 from django.template import loader
+from .models import map
 
 def index(request):
-    latest_question_list = Question.objects.order_by('-pub_date')[:5]
-    template = loader.get_template('map/index.html')
+    squirrels = map.objects.all()
     context = {
-        'latest_question_list': latest_question_list,
-    }
-    return HttpResponse(template.render(context, request))
-
-def index(request):
-    return render(request,'map/index.html') 
+            squirrels: 'squirrels'
+            }
+    return render(request,'map/index.html',context) 
